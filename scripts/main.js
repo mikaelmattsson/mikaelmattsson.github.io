2f71148f82e7694576c244796ef4f7026c370bf6
Vincent Klaiber <vincent@schimpanz.com>
2014-08-11 21:39:58 +0200

    Add back colors.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 (function() {
 
+  var a = ['red', 'blue', 'green', 'purple'];
+
   window.addEventListener('click', function(e) {
     e.preventDefault();
 
-    document.body.className = colors[Math.round(Math.random() * (colors.length-1))];
+    document.body.className = a[Math.round(Math.random() * (a.length - 1))];
   });
 
 }());
