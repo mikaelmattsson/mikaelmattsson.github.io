2e7d7e85ec3ae24ce3feb32586838d39eb1bd8e2
Vincent Klaiber <vincent@schimpanz.com>
2014-08-16 18:24:28 +0200

    Make it reponsive.

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,8 @@
 (function() {
-
-  var a = ['red', 'blue', 'green', 'purple'];
-
-  document.getElementsByTagName('header')[0].addEventListener('click', function(e) {
-    e.preventDefault();
-
-    document.body.className = a[Math.round(Math.random() * (a.length - 1))];
-  });
-
   var images = document.querySelectorAll('article img'),
       i = images.length;
 
   while (i--) {
     images[i].parentNode.style.border = 'none';
   }
-
 })();
